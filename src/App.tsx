b276620e0a42c9e475a8c8de51c1dc89baa796f6
Shunei Hayakawa <info@shuneihayakawa.biz>
2021-06-19 22:15:13 +0900

    added sample logo display

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,11 @@
 import React from 'react';
 
+import logo from './assets/logos/symbol_only.svg';
+
 export const App: React.VFC = () => {
-  return <div>Hello world!</div>;
+  return (
+    <div>
+      <img src={logo} />
+    </div>
+  );
 };
