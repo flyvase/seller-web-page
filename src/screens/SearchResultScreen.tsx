1b9510d5dd25e56ff7f7ff4a0637a7fc4dd77581
Shunei Hayakawa <info@shuneihayakawa.biz>
2021-06-21 23:30:07 +0900

    removed unnecessary space for app bar overflow and use padding instead

--- src/screens/SearchResultScreen.tsx
+++ src/screens/SearchResultScreen.tsx
@@ -1,6 +1,30 @@
-import { Box } from '@material-ui/core';
+import { Grid, makeStyles, Box } from '@material-ui/core';
 import React from 'react';
 
+import { SpaceList } from '../components/search_result_screen/SpaceList';
+
+const useStyles = makeStyles((theme) => {
+  return {
+    root: {
+      height: '100vh',
+      paddingTop: 64,
+    },
+  };
+});
+
 export const SearchResultScreen: React.VFC = () => {
-  return <Box>Search</Box>;
+  const classes = useStyles();
+
+  return (
+    <Grid container className={classes.root}>
+      <Grid item md={5}>
+        <Box p={2}>
+          <SpaceList />
+        </Box>
+      </Grid>
+      <Grid item md={7}>
+        map
+      </Grid>
+    </Grid>
+  );
 };
