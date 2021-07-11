7d3b33f2265abf2ab968ad74017d6201453b23de
ramihorii <ramihorii@r.recruit.co.jp>
2021-07-11 09:36:55 +0900

    [WIP] Add Areas Component

--- src/components/space_details_screen/Areas.tsx
+++ src/components/space_details_screen/Areas.tsx
@@ -1,10 +1,30 @@
-import { Box, Typography } from '@material-ui/core';
 import React from 'react';
+import { LoadScript, GoogleMap } from '@react-google-maps/api';
+import { Box, Typography } from '@material-ui/core';
+import { makeStyles } from '@material-ui/core';
+
+const useStyles = makeStyles(() => {
+  return {
+    mapContainer: {
+      height: '100%',
+      width: '100%',
+    },
+  };
+});
 
 export const Areas: React.VFC = () => {
+  const classes = useStyles();
+
   return (
     <Box>
       <Typography variant="h4">エリア</Typography>
+      <LoadScript googleMapsApiKey="AIzaSyB80FmCCcA4FjAZkx_a8Z--tCg_LcgaCiw">
+        <GoogleMap
+          center={{ lat: 35.69575, lng: 139.77521 }}
+          zoom={16}
+          mapContainerClassName={classes.mapContainer}
+        />
+      </LoadScript>
     </Box>
   );
 };
