8f7a6702231c626c33015332b245945b9f497634
Shunei <info@shuneihayakawa.biz>
2021-07-15 18:06:34 +0900

    initialized firebase app on entry point

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import firebase from 'firebase/app';
 
 import { App } from './App';
+
+firebase.initializeApp({
+  apiKey: 'AIzaSyARAPo5YFMO2A1nXbzf2byYAdginW-KC1A',
+  authDomain: 'flyvase-dev.firebaseapp.com',
+  projectId: 'flyvase-dev',
+  storageBucket: 'flyvase-dev.appspot.com',
+  messagingSenderId: '562369201187',
+  appId: '1:562369201187:web:4c1bd1f4a5fb40f6759d96',
+  measurementId: 'G-F25VG0R0G4',
+});
 
 ReactDOM.render(<App />, document.getElementById('root'));
