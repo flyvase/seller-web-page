2edfd32e6858b04cf8fee7ee97e976c4a05b84a9
Shunei <info@shuneihayakawa.biz>
2021-07-30 19:38:08 +0900

    added CountryPicker component

---
+++ src/components/common/CountryPicker.tsx
@@ -0,0 +1,51 @@
+import React from 'react';
+import { Autocomplete } from '@material-ui/lab';
+import { Box, TextField } from '@material-ui/core';
+
+import { countries, Country } from '../../config/country';
+
+function countryToFlag(isoCode: string) {
+  return typeof String.fromCodePoint !== 'undefined'
+    ? isoCode
+        .toUpperCase()
+        .replace(/./g, (char) =>
+          String.fromCodePoint(char.charCodeAt(0) + 127397)
+        )
+    : isoCode;
+}
+
+type CountryPickerProps = {
+  placeHolder?: string;
+  inputVariant?: 'filled' | 'outlined' | 'standard';
+};
+
+export const CountryPicker: React.VFC<CountryPickerProps> = (
+  props: CountryPickerProps
+) => {
+  return (
+    <Autocomplete
+      autoHighlight
+      options={countries as Country[]}
+      getOptionLabel={(option) =>
+        countryToFlag(option.code) + ' ' + option.label
+      }
+      renderOption={(option) => (
+        <Box>
+          <span>{countryToFlag(option.code) + ' '}</span>
+          {option.label} ({option.code}) +{option.phone}
+        </Box>
+      )}
+      renderInput={(params) => (
+        <TextField
+          {...params}
+          label={props.placeHolder ?? '国を選択してください'}
+          variant={props.inputVariant ?? 'outlined'}
+          inputProps={{
+            ...params.inputProps,
+            autoComplete: 'new-password', // disable autocomplete and autofill
+          }}
+        />
+      )}
+    />
+  );
+};
