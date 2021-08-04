c0122f71efa1532fc000c772531d21c1a6b80eae
Shunei <info@shuneihayakawa.biz>
2021-08-04 11:38:09 +0900

    extract form on SignUpScreen as a component

--- src/screens/SignUpScreen.tsx
+++ src/screens/SignUpScreen.tsx
@@ -1,17 +1,8 @@
-import {
-  Box,
-  Button,
-  makeStyles,
-  TextField,
-  Typography,
-} from '@material-ui/core';
-import { useFormik } from 'formik';
+import { Box, makeStyles, Typography } from '@material-ui/core';
 import React from 'react';
-import { object, string } from 'yup';
 
 import horizontalLogo from '../assets/logos/horizontal.svg';
-import { CountryPicker } from '../components/common/CountryPicker';
-import { countries } from '../config/country';
+import { SignUpForm } from '../components/sign_up_screen/SignUpForm';
 
 const useStyles = makeStyles((theme) => {
   return {
@@ -21,7 +12,7 @@ const useStyles = makeStyles((theme) => {
       justifyContent: 'center',
       alignItems: 'center',
     },
-    form: {
+    container: {
       padding: '0px 32px 24px 32px',
       maxWidth: 400,
       [theme.breakpoints.up('sm')]: {
@@ -34,115 +25,22 @@ const useStyles = makeStyles((theme) => {
     title: {
       paddingBottom: 16,
     },
-    firstNameInput: {
-      paddingBottom: 16,
-    },
-    lastNameInput: {
-      paddingBottom: 32,
-    },
-    phoneInputs: {
-      paddingBottom: 32,
-      display: 'flex',
-    },
-    countryPicker: {
-      minWidth: 80,
-      width: '30%',
-    },
-    phoneInput: {
-      paddingLeft: '16px',
-      width: '70%',
-    },
-    submitButton: {
-      padding: '8px 0px',
-    },
   };
-});
-
-const validationSchema = object({
-  country: object({
-    code: string(),
-    label: string(),
-    phone: string(),
-  }),
-  phoneNumber: string().required('必ず入力してください'),
 });
 
 export const SignUpScreen: React.VFC = () => {
   const classes = useStyles();
 
-  const formControl = useFormik({
-    initialValues: {
-      country: countries[112],
-      phoneNumber: '',
-    },
-    validationSchema: validationSchema,
-    onSubmit: (values) => {
-      console.log(values);
-    },
-  });
-
   return (
     <Box className={classes.root}>
-      <Box className={classes.form}>
+      <Box className={classes.container}>
         <img src={horizontalLogo} />
 
         <Typography className={classes.title} variant="h5" align="center">
           アカウントを作成
         </Typography>
 
-        <form onSubmit={formControl.handleSubmit}>
-          <TextField
-            className={classes.firstNameInput}
-            fullWidth
-            label="姓 *"
-          />
-
-          <TextField className={classes.lastNameInput} fullWidth label="名 *" />
-
-          <Box className={classes.phoneInputs}>
-            <Box className={classes.countryPicker}>
-              <CountryPicker
-                onChange={(country) => {
-                  formControl.setFieldValue(
-                    'country',
-                    country ?? formControl.initialValues.country
-                  );
-                }}
-                value={formControl.values.country}
-                id="countryPicker"
-              />
-            </Box>
-            <Box className={classes.phoneInput}>
-              <TextField
-                fullWidth
-                label="電話番号 *"
-                variant="outlined"
-                id="phoneNumber"
-                value={formControl.values.phoneNumber}
-                onChange={formControl.handleChange}
-                error={
-                  formControl.touched.phoneNumber &&
-                  Boolean(formControl.errors.phoneNumber)
-                }
-                helperText={
-                  formControl.touched.phoneNumber &&
-                  formControl.errors.phoneNumber
-                }
-              />
-            </Box>
-          </Box>
-
-          <Button
-            className={classes.submitButton}
-            variant="contained"
-            color="primary"
-            fullWidth
-            disableElevation
-            type="submit"
-          >
-            作成
-          </Button>
-        </form>
+        <SignUpForm />
       </Box>
     </Box>
   );
