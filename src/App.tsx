bebed20c4eb1ab94a2988068745092fd7d792998
Shunei <info@shuneihayakawa.biz>
2021-08-09 17:38:10 +0900

    added LoadingScreen on App.tsx while firebase authentication is in initialization

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,12 @@ import React, { useContext } from 'react';
 
 import { useAuthObserver } from './controllers/hooks/common/authController';
 import { AuthRepositoryContext } from './repositories/authRepository';
+import { LoadingScreen } from './screens/LoadingScreen';
 import { SignUpScreen } from './screens/SignUpScreen';
 
 export const App: React.VFC = () => {
   const authRepository = useContext(AuthRepositoryContext);
-  useAuthObserver(authRepository);
+  const initialized = useAuthObserver(authRepository);
 
-  return <SignUpScreen />;
+  return initialized ? <SignUpScreen /> : <LoadingScreen />;
 };
