ee859e43f7f3d8eed65a59b393d96959822cb7fe
Shunei <info@shuneihayakawa.biz>
2021-08-09 10:55:05 +0900

    added custom InvalidArgumentError class

---
+++ src/core/errors.ts
@@ -0,0 +1,20 @@
+export class InvalidArgumentError implements Error {
+  name: string;
+  message: string;
+  stack?: string;
+
+  constructor(
+    errors: { [key: string]: string },
+    className: string,
+    stack?: string
+  ) {
+    this.name = new.target.name;
+    this.message =
+      `Invalid arguments on instantiation of class "${className}".` +
+      '\n' +
+      Object.entries(errors)
+        .map(([key, val]) => `Key: "${key}", message: "${val}"`)
+        .join('\n');
+    this.stack = stack;
+  }
+}
