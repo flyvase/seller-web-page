d847727f6e6299a4fb0067d75296109d0e9b3725
Shunei <info@shuneihayakawa.biz>
2021-08-09 11:56:19 +0900

    moved root wrapper components to main.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,7 @@
-import { ThemeProvider } from '@material-ui/core';
-import { RecoilRoot } from 'recoil';
 import React from 'react';
 
 import { SignUpScreen } from './screens/SignUpScreen';
-import { theme } from './theme';
 
 export const App: React.VFC = () => {
-  return (
-    <RecoilRoot>
-      <ThemeProvider theme={theme}>
-        <SignUpScreen />
-      </ThemeProvider>
-    </RecoilRoot>
-  );
+  return <SignUpScreen />;
 };
