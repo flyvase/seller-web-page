6944304bec649f204dcfd656985c15cce01997f9
Shunei <info@shuneihayakawa.biz>
2021-08-09 16:06:26 +0900

    added authObserver invocation on entrypoint

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,12 @@
-import React from 'react';
+import React, { useContext } from 'react';
 
+import { useAuthObserver } from './controllers/hooks/common/authController';
+import { AuthRepositoryContext } from './repositories/authRepository';
 import { SignUpScreen } from './screens/SignUpScreen';
 
 export const App: React.VFC = () => {
+  const authRepository = useContext(AuthRepositoryContext);
+  useAuthObserver(authRepository);
+
   return <SignUpScreen />;
 };
