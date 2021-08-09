96708dece784c7a81e876244f9a44e339c939cb3
Shunei <info@shuneihayakawa.biz>
2021-08-09 09:48:48 +0900

    changed state management on SignUpScreen to recoil

--- src/screens/SignUpScreen.tsx
+++ src/screens/SignUpScreen.tsx
@@ -1,8 +1,13 @@
 import { Box, makeStyles, Typography } from '@material-ui/core';
+import { useRecoilState } from 'recoil';
 import React from 'react';
 
 import horizontalLogo from '../assets/logos/horizontal.svg';
+import { PinCodeForm } from '../components/sign_up_screen/PinCodeForm';
 import { SignUpForm } from '../components/sign_up_screen/SignUpForm';
+import { formModeState } from '../controllers/state/sign_up_screen/formModeState';
+
+export type SignUpFormMode = 'signUp' | 'pinCode';
 
 const useStyles = makeStyles((theme) => {
   return {
@@ -14,7 +19,7 @@ const useStyles = makeStyles((theme) => {
     },
     container: {
       padding: '0px 32px 24px 32px',
-      maxWidth: 400,
+      width: 400,
       [theme.breakpoints.up('sm')]: {
         borderWidth: 2,
         borderColor: theme.palette.divider,
@@ -30,6 +35,7 @@ const useStyles = makeStyles((theme) => {
 
 export const SignUpScreen: React.VFC = () => {
   const classes = useStyles();
+  const [formMode, setFormMode] = useRecoilState(formModeState);
 
   return (
     <Box className={classes.root}>
@@ -37,10 +43,12 @@ export const SignUpScreen: React.VFC = () => {
         <img src={horizontalLogo} />
 
         <Typography className={classes.title} variant="h5" align="center">
-          アカウントを作成
+          {formMode === 'signUp'
+            ? 'アカウントを作成'
+            : 'SMSに送信された6桁の番号を入力してください'}
         </Typography>
 
-        <SignUpForm />
+        {formMode === 'signUp' ? <SignUpForm /> : <PinCodeForm />}
       </Box>
     </Box>
   );
