bdc2350bb828afe2309d2ac0bd8387eec6410ef9
Shunei <info@shuneihayakawa.biz>
2021-08-09 16:05:39 +0900

    added method to subscribe to auth stream of firebase on AuthRepository

--- src/repositories/authRepository.ts
+++ src/repositories/authRepository.ts
@@ -1,12 +1,24 @@
 import firebase from 'firebase';
 import { createContext } from 'react';
 
+import { Auth } from '../entities/auth';
 import { AuthInterface } from '../interfaces/authInterface';
 
 export class AuthRepository implements AuthInterface {
   async googleSignIn(): Promise<void> {
     const provider = new firebase.auth.GoogleAuthProvider();
     await firebase.auth().signInWithRedirect(provider);
+  }
+
+  authObserver(callback: (auth: Auth | null) => void): () => void {
+    const cancel = firebase.auth().onAuthStateChanged((user) => {
+      if (user == null) {
+        callback(null);
+      } else {
+        callback(new Auth(user.uid));
+      }
+    });
+    return () => cancel();
   }
 }
 
