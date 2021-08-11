7fda26195b32178c73a95f53200be555cc1e0cd6
Shunei <info@shuneihayakawa.biz>
2021-08-11 13:39:21 +0900

    added Omit type alias

---
+++ src/core/types.ts
@@ -0,0 +1,3 @@
+// Use this type if you want to remove some properties from predefined prop types.
+// ex. remove variant property from TypographyProps on Material UI
+export type Omit<T, K extends keyof T> = Pick<T, Exclude<keyof T, K>>;
