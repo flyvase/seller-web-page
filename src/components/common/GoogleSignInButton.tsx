55254ecd1d325783634c7dbeb15188718df3943a
Shunei <info@shuneihayakawa.biz>
2021-08-11 13:39:39 +0900

    added GoogleSignInButton component

---
+++ src/components/common/GoogleSignInButton.tsx
@@ -0,0 +1,38 @@
+import { Button, ButtonProps, makeStyles } from '@material-ui/core';
+import React from 'react';
+
+import googleLogo from '../../assets/logos/google_logo.svg';
+
+const useStyles = makeStyles(() => ({
+  button: {
+    textTransform: 'none',
+    backgroundColor: 'white',
+    paddingLeft: '6px',
+  },
+  logo: {
+    // disable default margin
+    marginRight: '0px',
+  },
+}));
+
+type GoogleSignInButtonProps = Omit<ButtonProps, 'variant' | 'startIcon'>;
+
+export const GoogleSignInButton: React.VFC<GoogleSignInButtonProps> = (
+  props: GoogleSignInButtonProps
+) => {
+  const classes = useStyles();
+
+  return (
+    <Button
+      {...props}
+      variant="outlined"
+      startIcon={<img src={googleLogo} />}
+      className={classes.button}
+      classes={{
+        startIcon: classes.logo,
+      }}
+    >
+      Sign in with Google
+    </Button>
+  );
+};
