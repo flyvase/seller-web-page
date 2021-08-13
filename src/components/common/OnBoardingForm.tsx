454b068d888597a307bde14282c53d92870dd9d3
Shunei <info@shuneihayakawa.biz>
2021-08-13 10:43:47 +0900

    added reusable component for on boarding process

---
+++ src/components/common/OnBoardingForm.tsx
@@ -0,0 +1,51 @@
+import { Box, makeStyles, Typography } from '@material-ui/core';
+import React, { ReactNode } from 'react';
+
+import horizontalLogo from '../../assets/logos/horizontal.svg';
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    height: '100vh',
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  container: {
+    padding: '0px 32px 24px 32px',
+    width: 400,
+    [theme.breakpoints.up('sm')]: {
+      borderWidth: 2,
+      borderColor: theme.palette.divider,
+      borderStyle: 'solid',
+      borderRadius: 24,
+    },
+  },
+  title: {
+    paddingBottom: 16,
+  },
+}));
+
+type OnBoardingFormProps = {
+  children: ReactNode;
+  title: string;
+};
+
+export const OnBoardingForm: React.VFC<OnBoardingFormProps> = (
+  props: OnBoardingFormProps
+) => {
+  const classes = useStyles();
+
+  return (
+    <Box className={classes.root}>
+      <Box className={classes.container}>
+        <img src={horizontalLogo} />
+
+        <Typography className={classes.title} variant="h5" align="center">
+          {props.title}
+        </Typography>
+
+        {props.children}
+      </Box>
+    </Box>
+  );
+};
