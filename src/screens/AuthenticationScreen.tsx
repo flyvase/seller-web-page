8bfe8cc09cf652680d69d6e78008a61c3eb0b068
Shunei <info@shuneihayakawa.biz>
2021-08-13 10:44:10 +0900

    modified existing forms to use OnBoardingForm component

--- src/screens/AuthenticationScreen.tsx
+++ src/screens/AuthenticationScreen.tsx
@@ -1,40 +1,15 @@
-import { Box, makeStyles, Typography } from '@material-ui/core';
 import { useHistory } from 'react-router-dom';
 import React, { useContext, useEffect } from 'react';
 
 import { GoogleSignInButton } from '../components/common/GoogleSignInButton';
-import horizontalLogo from '../assets/logos/horizontal.svg';
 import {
   useAuthResult,
   useGoogleSignIn,
 } from '../controllers/hooks/common/authController';
 import { AuthRepositoryContext } from '../repositories/authRepository';
-
-const useStyles = makeStyles((theme) => ({
-  root: {
-    height: '100vh',
-    display: 'flex',
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-  container: {
-    padding: '0px 32px 24px 32px',
-    width: 400,
-    [theme.breakpoints.up('sm')]: {
-      borderWidth: 2,
-      borderColor: theme.palette.divider,
-      borderStyle: 'solid',
-      borderRadius: 24,
-    },
-  },
-  title: {
-    paddingBottom: 16,
-  },
-}));
+import { OnBoardingForm } from '../components/common/OnBoardingForm';
 
 export const AuthenticationScreen: React.VFC = () => {
-  const classes = useStyles();
-
   const authRepository = useContext(AuthRepositoryContext);
   const googleSignIn = useGoogleSignIn(authRepository);
   const authResult = useAuthResult(authRepository);
@@ -52,21 +27,13 @@ export const AuthenticationScreen: React.VFC = () => {
   }, []);
 
   return (
-    <Box className={classes.root}>
-      <Box className={classes.container}>
-        <img src={horizontalLogo} />
-
-        <Typography className={classes.title} variant="h5" align="center">
-          サインイン、サインアップ
-        </Typography>
-
-        <GoogleSignInButton
-          fullWidth
-          onClick={async () => {
-            await googleSignIn();
-          }}
-        />
-      </Box>
-    </Box>
+    <OnBoardingForm title="サインイン、サインアップ">
+      <GoogleSignInButton
+        fullWidth
+        onClick={async () => {
+          await googleSignIn();
+        }}
+      />
+    </OnBoardingForm>
   );
 };
--- src/screens/SignUpScreen.tsx
+++ src/screens/SignUpScreen.tsx
@@ -1,69 +1,38 @@
-import { Box, makeStyles, Typography } from '@material-ui/core';
 import { useRecoilState } from 'recoil';
 import React, { useRef } from 'react';
 
-import horizontalLogo from '../assets/logos/horizontal.svg';
 import { PinCodeForm } from '../components/sign_up_screen/PinCodeForm';
 import { SignUpForm } from '../components/sign_up_screen/SignUpForm';
 import { formModeState } from '../controllers/state/sign_up_screen/formModeState';
 import { User } from '../entities/user';
+import { OnBoardingForm } from '../components/common/OnBoardingForm';
 
 export type SignUpFormMode = 'signUp' | 'pinCode';
 
-const useStyles = makeStyles((theme) => {
-  return {
-    root: {
-      height: '100vh',
-      display: 'flex',
-      justifyContent: 'center',
-      alignItems: 'center',
-    },
-    container: {
-      padding: '0px 32px 24px 32px',
-      width: 400,
-      [theme.breakpoints.up('sm')]: {
-        borderWidth: 2,
-        borderColor: theme.palette.divider,
-        borderStyle: 'solid',
-        borderRadius: 24,
-      },
-    },
-    title: {
-      paddingBottom: 16,
-    },
-  };
-});
-
 export const SignUpScreen: React.VFC = () => {
-  const classes = useStyles();
-
   const [formMode] = useRecoilState(formModeState);
 
   const userRef = useRef<User>(null!);
   const phoneNumberRef = useRef<string>(null!);
 
   return (
-    <Box className={classes.root}>
-      <Box className={classes.container}>
-        <img src={horizontalLogo} />
-
-        <Typography className={classes.title} variant="h5" align="center">
-          {formMode === 'signUp'
-            ? 'アカウントを作成'
-            : 'SMSに送信された6桁の番号を入力してください'}
-        </Typography>
-
-        {formMode === 'signUp' ? (
-          <SignUpForm
-            onSubmit={(firstName, lastName, phoneNumber) => {
-              userRef.current = new User(firstName, lastName);
-              phoneNumberRef.current = phoneNumber;
-            }}
-          />
-        ) : (
-          <PinCodeForm />
-        )}
-      </Box>
-    </Box>
+    <OnBoardingForm
+      title={
+        formMode === 'signUp'
+          ? 'アカウントを作成'
+          : 'SMSに送信された6桁の番号を入力してください'
+      }
+    >
+      {formMode === 'signUp' ? (
+        <SignUpForm
+          onSubmit={(firstName, lastName, phoneNumber) => {
+            userRef.current = new User(firstName, lastName);
+            phoneNumberRef.current = phoneNumber;
+          }}
+        />
+      ) : (
+        <PinCodeForm />
+      )}
+    </OnBoardingForm>
   );
 };
