692c501aa8928cd38200af9f45fbfec7f7cc64a7
Shunei <info@shuneihayakawa.biz>
2021-08-19 16:20:32 +0900

    added re authentication handling on PhoneNumberEnrollmentScreen

--- src/screens/PhoneNumberEnrollmentScreen.tsx
+++ src/screens/PhoneNumberEnrollmentScreen.tsx
@@ -1,12 +1,21 @@
-import React from 'react';
+import React, { useContext } from 'react';
 
 import { GoogleSignInButton } from '../components/common/GoogleSignInButton';
 import { LogoForm } from '../components/common/LogoForm';
+import {
+  useHandleReAuthenticationResult,
+  useReAuthenticateWithGoogle,
+} from '../controllers/hooks/common/authController';
+import { AuthRepositoryContext } from '../repositories/authRepository';
 
 export const PhoneNumberEnrollmentScreen: React.VFC = () => {
+  const authRepository = useContext(AuthRepositoryContext);
+  const reAuthenticateWithGoogle = useReAuthenticateWithGoogle(authRepository);
+  useHandleReAuthenticationResult(authRepository);
+
   return (
     <LogoForm title="電話番号を登録するには再度ログインが必要です">
-      <GoogleSignInButton fullWidth />
+      <GoogleSignInButton fullWidth onClick={reAuthenticateWithGoogle} />
     </LogoForm>
   );
 };
