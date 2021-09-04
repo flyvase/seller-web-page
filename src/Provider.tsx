9f16340c88dcf20532e11f47542d5aa5c66dc53d
Shunei <info@shuneihayakawa.biz>
2021-09-04 18:57:55 +0900

    added RecoilRoot and ThemeProvider to root component

--- src/Provider.tsx
+++ src/Provider.tsx
@@ -1,5 +1,16 @@
+import { ThemeProvider } from '@material-ui/core';
 import React from 'react';
+import { RecoilRoot } from 'recoil';
+
+import { App } from './presenter/App';
+import { theme } from './theme';
 
 export const Provider: React.VFC = () => {
-  return <div></div>;
+  return (
+    <RecoilRoot>
+      <ThemeProvider theme={theme}>
+        <App />
+      </ThemeProvider>
+    </RecoilRoot>
+  );
 };
