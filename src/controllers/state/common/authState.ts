73c73239dbc92193e69ddc5a79dce7fabf87a021
Shunei <info@shuneihayakawa.biz>
2021-09-04 18:43:49 +0900

    removed previous codes

--- src/controllers/state/common/authState.ts
+++
@@ -1,8 +0,0 @@
-import { atom } from 'recoil';
-
-import { Auth } from '../../../entities/auth';
-
-export const authState = atom<Auth | null>({
-  key: 'authState',
-  default: null,
-});
--- src/theme.ts
+++
@@ -1,9 +0,0 @@
-import { createTheme } from '@material-ui/core';
-
-export const theme = createTheme({
-  palette: {
-    primary: {
-      main: '#f7b916',
-    },
-  },
-});
