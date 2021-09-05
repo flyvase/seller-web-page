54716f60749b6ab022afd3a07b294b880f8e3ac4
Shunei <info@shuneihayakawa.biz>
2021-09-05 15:14:43 +0900

    added implementation of WebClient interface

---
+++ src/infrastructure/repository/webClientImpl.ts
@@ -0,0 +1,20 @@
+import { serverDomain } from '../../config/http';
+import { WebClient, WebRequest } from '../../domain/repository/webClient';
+
+export class WebClientImpl implements WebClient {
+  async execute<T extends WebRequest>(request: T): Promise<Response> {
+    const url = new URL(request.path, serverDomain);
+    return fetch(url.toString(), {
+      method: request.options.httpMethod,
+      mode: request.options.mode,
+      headers:
+        request.options.httpHeaders != undefined
+          ? Array.from(request.options.httpHeaders)
+          : undefined,
+      body:
+        request.options.body != undefined
+          ? JSON.stringify(Object.fromEntries(request.options.body))
+          : undefined,
+    });
+  }
+}
