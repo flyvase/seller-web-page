72894d8351a593e58895a06d80155b6bc16a82e4
Shunei <info@shuneihayakawa.biz>
2021-09-05 15:15:50 +0900

    added new repositories as the providers on root compoent

--- src/Provider.tsx
+++ src/Provider.tsx
@@ -3,17 +3,25 @@ import React from 'react';
 import { RecoilRoot } from 'recoil';
 
 import { authRepositoryContext } from './domain/repository/authRepository';
+import { userRepositoryContext } from './domain/repository/userRepository';
 import { AuthRepositoryImpl } from './infrastructure/repository/authRepositoryImpl';
+import { UserRepositoryImpl } from './infrastructure/repository/userRepositoryImpl';
+import { WebClientImpl } from './infrastructure/repository/webClientImpl';
 import { App } from './presenter/App';
 import { theme } from './theme';
 
 export const Provider: React.VFC = () => {
   const authRepositoryImpl = new AuthRepositoryImpl();
+  const webClientImpl = new WebClientImpl();
+  const userRepositoryImpl = new UserRepositoryImpl(webClientImpl);
+
   return (
     <RecoilRoot>
       <ThemeProvider theme={theme}>
         <authRepositoryContext.Provider value={authRepositoryImpl}>
-          <App />
+          <userRepositoryContext.Provider value={userRepositoryImpl}>
+            <App />
+          </userRepositoryContext.Provider>
         </authRepositoryContext.Provider>
       </ThemeProvider>
     </RecoilRoot>
