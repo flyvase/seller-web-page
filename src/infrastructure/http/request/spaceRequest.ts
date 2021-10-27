87a7de713175cfcc6fded99ce87f418b0fc1f8ae
shou-watanabe <watanabeshou1520@gmail.com>
2021-10-27 10:55:33 +0900

    FVSRTL-94 add buildGetDefaultHeader

--- src/infrastructure/http/request/spaceRequest.ts
+++ src/infrastructure/http/request/spaceRequest.ts
@@ -1,12 +1,12 @@
 import { HttpRequest } from '../core/httpClient';
-import { buildPostDefaultHeader } from './helper';
+import { buildGetDefaultHeader } from './helper';
 
 export class SpaceGetRequest extends HttpRequest {
   constructor(authToken: string) {
     super('spaces/', {
       mode: 'cors',
       httpMethod: 'GET',
-      httpHeaders: buildPostDefaultHeader(authToken),
+      httpHeaders: buildGetDefaultHeader(authToken),
     });
   }
 }
