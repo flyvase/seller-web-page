64bc6221b39ae09dce52cf4a505d22719b502756
shou-watanabe <watanabeshou1520@gmail.com>
2021-11-21 17:56:00 +0900

    FVSRT-96 add typography theme

--- src/theme.ts
+++ src/theme.ts
@@ -3,7 +3,22 @@ import { createTheme } from '@mui/material';
 export const theme = createTheme({
   palette: {
     primary: {
-      main: '#f7b916',
+      main: '#3EBC9E',
+    },
+  },
+  typography: {
+    fontFamily: 'Hiragino Kaku Gothic ProN',
+    h2: {
+      fontSize: '48px',
+      fontColor: '#333333',
+    },
+    h4: {
+      fontSize: '24px',
+      fontColor: '#333333',
+    },
+    subtitle1: {
+      fontSize: '16px',
+      fontColor: '#333333',
     },
   },
 });
