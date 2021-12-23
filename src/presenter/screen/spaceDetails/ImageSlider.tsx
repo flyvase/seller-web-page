3507d02dac752d7ded9dd193546b53795a63263a
Shunei <info@shuneihayakawa.biz>
2021-12-23 11:46:46 +0900

    added arrow buttons on ImageSlider

--- src/presenter/screen/spaceDetails/ImageSlider.tsx
+++ src/presenter/screen/spaceDetails/ImageSlider.tsx
@@ -1,7 +1,9 @@
-import { Box, styled } from '@mui/material';
+import { Box, Button, styled } from '@mui/material';
 import React from 'react';
 
 import { SpaceImage } from '../../../domain/model/spaceImage';
+import leftArrow from '../../../assets/left_arrow.svg';
+import rightArrow from '../../../assets/right_arrow.svg';
 
 type ImageSliderProps = {
   spaceImages: SpaceImage[];
@@ -12,6 +14,37 @@ const RootBox = styled(Box)(({ theme }) => ({
   aspectRatio: '1.3',
   [theme.breakpoints.up('sm')]: {
     aspectRatio: '2',
+  },
+  position: 'relative',
+}));
+
+const LeftArrowButtonWrapper = styled(Box)(() => ({
+  position: 'absolute',
+  height: '100%',
+  left: 0,
+  display: 'flex',
+  alignItems: 'center',
+}));
+
+const RightArrowButtonWrapper = styled(Box)(() => ({
+  position: 'absolute',
+  height: '100%',
+  right: 0,
+  display: 'flex',
+  alignItems: 'center',
+}));
+
+const ArrowButton = styled(Button)(() => ({
+  // disable hover effect
+  '&:hover': {
+    background: 'none',
+  },
+}));
+
+const ArrowImg = styled('img')(({ theme }) => ({
+  height: '20px',
+  [theme.breakpoints.up('sm')]: {
+    height: '32px',
   },
 }));
 
@@ -41,6 +74,16 @@ export const ImageSlider: React.VFC<ImageSliderProps> = (
 ) => {
   return (
     <RootBox>
+      <LeftArrowButtonWrapper>
+        <ArrowButton disableFocusRipple disableRipple disableTouchRipple>
+          <ArrowImg src={leftArrow} />
+        </ArrowButton>
+      </LeftArrowButtonWrapper>
+      <RightArrowButtonWrapper>
+        <ArrowButton disableFocusRipple disableRipple disableTouchRipple>
+          <ArrowImg src={rightArrow} />
+        </ArrowButton>
+      </RightArrowButtonWrapper>
       <Carousel>
         {props.spaceImages.map((i) => (
           <Image src={i.imageUrl} key={i.id.value} />
