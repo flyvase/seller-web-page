05a7a6349047ef0b72b6505fe524f7cb3660278e
Shunei <info@shuneihayakawa.biz>
2021-12-28 19:42:09 +0900

    added swiper.js to SpaceDetailsScreen

--- src/presenter/screen/spaceDetails/SpaceImageSlider.tsx
+++ src/presenter/screen/spaceDetails/SpaceImageSlider.tsx
@@ -1,12 +1,25 @@
 import { styled } from '@mui/material';
 import React from 'react';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { Navigation, Pagination } from 'swiper';
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
 
 import { SpaceImage } from '../../../domain/model/spaceImage';
-import { CarouselSlider } from '../../component/CarouselSlider';
 
 type ImageSliderProps = {
   spaceImages: SpaceImage[];
 };
+
+const ImageSlider = styled(Swiper)(({ theme }) => ({
+  width: '100%',
+  aspectRatio: '1.3',
+  position: 'relative',
+  [theme.breakpoints.up('sm')]: {
+    aspectRatio: '2',
+  },
+}));
 
 const Image = styled('img')(() => ({
   objectFit: 'cover',
@@ -18,12 +31,16 @@ export const SpaceImageSlider: React.VFC<ImageSliderProps> = (
   props: ImageSliderProps
 ) => {
   return (
-    <CarouselSlider
-      width="100%"
-      aspectRatio={{ xs: '1.3', sm: '2' }}
-      items={props.spaceImages.map((i) => (
-        <Image src={i.imageUrl} key={i.id.value} />
+    <ImageSlider
+      modules={[Pagination, Navigation]}
+      navigation
+      pagination={{ clickable: true }}
+    >
+      {props.spaceImages.map((i) => (
+        <SwiperSlide key={i.id.value}>
+          <Image src={i.imageUrl} />
+        </SwiperSlide>
       ))}
-    />
+    </ImageSlider>
   );
 };
