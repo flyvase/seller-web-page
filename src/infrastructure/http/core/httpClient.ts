2e1fe040cd58236138d0abc5b69bf102c8e75da3
Shunei <info@shuneihayakawa.biz>
2022-01-03 10:58:33 +0900

    modified HttpClient#execute to return error and ok response

--- src/infrastructure/http/core/httpClient.ts
+++ src/infrastructure/http/core/httpClient.ts
@@ -1,28 +1,58 @@
 import { serverDomain } from '../../../config/http';
+import { HttpError } from './httpError';
 import { HttpRequest } from './httpRequest';
 import { HttpResponse } from './httpResponse';
+
+type ExecuteResult<U> = {
+  response?: HttpResponse<U>;
+  isHttpError: boolean;
+  error?: HttpError;
+  isNetworkError: boolean;
+};
 
 export class HttpClient {
   async execute<T extends HttpRequest, U>(
     request: T
-  ): Promise<HttpResponse<U>> {
+  ): Promise<ExecuteResult<U>> {
     const url = new URL(request.path, serverDomain);
-    const response = await fetch(url.toString(), {
-      method: request.method,
-      mode: request.mode,
-      body:
-        request.body != undefined
-          ? JSON.stringify(Object.fromEntries(request.body))
-          : undefined,
-      headers:
-        request.headers != undefined ? Array.from(request.headers) : undefined,
-    });
+    try {
+      const response = await fetch(url.toString(), {
+        method: request.method,
+        mode: request.mode,
+        body:
+          request.body != undefined
+            ? JSON.stringify(Object.fromEntries(request.body))
+            : undefined,
+        headers:
+          request.headers != undefined
+            ? Array.from(request.headers)
+            : undefined,
+      });
 
-    const body: U = await response.json();
-    return new HttpResponse({
-      statusCode: response.status,
-      statusText: response.statusText,
-      body: body,
-    });
+      if (response.ok) {
+        return {
+          response: new HttpResponse({
+            statusCode: response.status,
+            body: request.method === 'GET' ? await response.json() : undefined,
+          }),
+          isHttpError: false,
+          isNetworkError: false,
+        };
+      }
+
+      return {
+        isHttpError: true,
+        isNetworkError: false,
+        error: new HttpError({
+          statusCode: response.status,
+          message: await response.text(),
+        }),
+      };
+    } catch (e) {
+      return {
+        isHttpError: false,
+        isNetworkError: true,
+      };
+    }
   }
 }
