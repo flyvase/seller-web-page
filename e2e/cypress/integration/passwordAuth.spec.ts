03dbe2a1d0e888b071f2d046b9f569cb5ae5ac96
Shunei <info@shuneihayakawa.biz>
2022-01-11 18:57:36 +0900

    added happy path test for password authentication

--- e2e/cypress/integration/passwordAuth.spec.ts
+++ e2e/cypress/integration/passwordAuth.spec.ts
@@ -1,3 +1,5 @@
+import { userAccount } from '../global/user';
+
 describe('password authentication tests', () => {
   before(() => {
     cy.task('user:create');
@@ -7,7 +9,16 @@ describe('password authentication tests', () => {
     cy.task('user:delete');
   });
 
-  it('successfully loads', () => {
+  it('tests password authentication works with valid input', () => {
     cy.visit('/');
+
+    cy.get('[data-test=email]').should('be.visible').type(userAccount.email);
+    cy.get('[data-test=password]')
+      .should('be.visible')
+      .type(userAccount.password);
+
+    cy.get('[data-test=submit]').should('be.visible').click();
+
+    expect(cy.url(), 'http://localhost:3000/');
   });
 });
