808a365a67a2bce4cf0cfda4223187259b38b26c
Shunei <info@shuneihayakawa.biz>
2022-01-11 18:55:35 +0900

    modified cypress plugin code to use global constant

---
+++ e2e/cypress/global/user.ts
@@ -0,0 +1,5 @@
+export const userAccount = {
+  email: 'test123456789@gmail.com',
+  password: 'password',
+  uid: 'aucRUB2JiV7MzFkaa8M4',
+};
--- e2e/cypress/plugins/index.ts
+++ e2e/cypress/plugins/index.ts
@@ -1,27 +1,28 @@
 import { initializeApp, cert } from 'firebase-admin/app';
 import { getAuth } from 'firebase-admin/auth';
+import { userAccount } from '../global/user';
 
 export default function (
   on: Cypress.PluginEvents,
   config: Cypress.PluginConfigOptions
 ): void | Cypress.ConfigOptions | Promise<Cypress.ConfigOptions> {
-  const email = 'test123456789@gmail.com';
-  const password = 'password';
-  const uid = 'aucRUB2JiV7MzFkaa8M4';
-
   initializeApp({
     credential: cert('seller-web-page-e2e-testing-auth-admin-sa-key.json'),
   });
 
   on('task', {
     async 'user:create'() {
-      await getAuth().createUser({ uid, email, password });
+      await getAuth().createUser({
+        uid: userAccount.uid,
+        email: userAccount.email,
+        password: userAccount.password,
+      });
 
       return null;
     },
 
     async 'user:delete'() {
-      await getAuth().deleteUser(uid);
+      await getAuth().deleteUser(userAccount.uid);
 
       return null;
     },
