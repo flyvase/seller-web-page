a8cd803da940be709cd7debc838e287257adaced
Shunei <info@shuneihayakawa.biz>
2022-01-11 19:02:26 +0900

    modified lint settings on e2e directory

--- e2e/cypress/plugins/index.ts
+++ e2e/cypress/plugins/index.ts
@@ -1,10 +1,10 @@
 import { initializeApp, cert } from 'firebase-admin/app';
 import { getAuth } from 'firebase-admin/auth';
+
 import { userAccount } from '../global/user';
 
 export default function (
-  on: Cypress.PluginEvents,
-  config: Cypress.PluginConfigOptions
+  on: Cypress.PluginEvents
 ): void | Cypress.ConfigOptions | Promise<Cypress.ConfigOptions> {
   initializeApp({
     credential: cert('seller-web-page-e2e-testing-auth-admin-sa-key.json'),
