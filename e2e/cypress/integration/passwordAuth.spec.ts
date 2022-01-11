8dafce6a43498c9eefaaf610ccb0f3fc586de95c
Shunei <info@shuneihayakawa.biz>
2022-01-11 19:14:06 +0900

    added test for invalid email input case on password authentication

--- e2e/cypress/integration/passwordAuth.spec.ts
+++ e2e/cypress/integration/passwordAuth.spec.ts
@@ -9,7 +9,7 @@ describe('password authentication tests', () => {
     cy.task('user:delete');
   });
 
-  it('tests password authentication works with valid input', () => {
+  it('tests auth works with valid input', () => {
     cy.visit('/');
 
     cy.get('[data-test=email]').should('be.visible').type(userAccount.email);
@@ -20,5 +20,24 @@ describe('password authentication tests', () => {
     cy.get('[data-test=submit]').should('be.visible').click();
 
     expect(cy.url(), 'http://localhost:3000/');
+  });
+
+  it("tests auth doesn't work with invalid email", () => {
+    cy.visit('/');
+
+    const email = 'invalid@gmail.com';
+
+    cy.get('[data-test=email]').should('be.visible').type(email);
+    cy.get('[data-test=password]')
+      .should('be.visible')
+      .type(userAccount.password);
+
+    cy.get('[data-test=submit]').should('be.visible').click();
+
+    expect(
+      cy
+        .get('[data-test=error]')
+        .contains(`${email}で登録されたユーザーが存在しません`)
+    );
   });
 });
